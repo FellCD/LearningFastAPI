99d94af3e0f637f9abe1d938ba2f44628f7da120
FellCD <felipegabrieldosantosrodrigues@gmail.com>
2026-09-14 23:06:41 -0300

    feat: endpoint POST adicionado!

--- learningFastAPI/errorTreatment.py
+++ learningFastAPI/errorTreatment.py
@@ -37,3 +37,81 @@ connection.commit()
 cursor.close()
 connection.close()
 
+class DurationSchema(BaseModel):
+    duration_min: int
+    duration_sec: int
+
+class SongSchema(BaseModel):
+    name: str
+    author: str
+    duration: DurationSchema
+
+
+app = FastAPI()
+
+
+# Verbo POST para adicionar músicas
+@app.post("/songs/", status_code=status.HTTP_201_CREATED)
+def add_song(song: SongSchema):
+
+    # Declaração das variáveis antes do Try
+    connection = None
+    cursor = None
+
+    try: # Final Bom
+
+        seconds_total = (song.duration.duration_min * 60) + song.duration.duration_sec
+
+        # Definição dos dados
+        commandSQL: str = """INSERT INTO songs (name, author, duration_second) VALUES (?, ?, ?);"""
+        dados: tuple[str, str, int] = (song.name, song.author, seconds_total)
+
+        # Abre conexão
+        connection = sqlite3.connect("songs.db")
+        cursor = connection.cursor()
+
+        # Executa comandos
+        cursor.execute(commandSQL, dados)
+
+        # Salva as informações
+        connection.commit()
+
+        return {
+            "status": "sucesso!",
+            "mensagem": f"A música {song.name} foi adicionada com todo o sucesso do mundo!"
+        }
+
+    # Final Ruim: Erro do banco
+    except sqlite3.Error as e:
+        if connection: # Se connection estiver ativa
+            connection.rollback() # Desfaz algo se deu errado no meio da execução
+
+        # Avisa ao cliente/frontend sobre o status da situação
+        raise HTTPException(
+            status_code= status.HTTP_400_BAD_REQUEST,
+            detail= f"Erro de requisição de inserção: {str(e)}",
+        )
+
+    # Final Ruim: Erro Genérico
+    except Exception as e:
+        if connection:
+            connection.rollback()
+
+        # Avisa ao cliente/frontend sobre o status da situação
+        raise HTTPException(
+            status_code= status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail= f"Erro Interno: {str(e)}",
+        )
+        
+
+    finally: # Inevitável
+
+        # Fecha o cursor
+        if cursor:
+            cursor.close()
+
+        # Fecha a conexão
+        if connection:
+            connection.close()
+
+
